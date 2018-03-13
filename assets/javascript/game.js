3760a53468541805f83b7e8e6d67b5e1f5b485e2
Tim Tucker <tntucker0015@outlook.com>
2018-03-13 14:00:36 -0400

    adding media files for sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,17 @@
 // check guessed letters against available letters
-// display turns left - for loop for counting down to 0
+// for loop stopping game when guessses left reaches 0 or correct guesses matches target word
 // need to add 1 to wins when word is guessed 
-//format start button
-// format guessNow button to allow user to guess the entire word at one time.
-// add images baseball coutner, win and loss
-// add sounds to strike and hit
 
+var hitSound = new Audio(src = 'assets/media/hit.mp3');
+var strikeSound = new Audio(src = 'assets/media/strike.mp3')
+var crowd = new Audio(src = 'assets/media/crowd.mp3');
+var lose = new Audio(src = "assets/media/alo-cry.wav");
 // list of team mascots
 var game = {
     teams: ['brewers', 'orioles', 'reds', 'angels', 'phillies', 'diamondbacks', 'redsox', 'indians', 'dodgers', 'pirates', 'cubs', 'whitesox', 'rockies', 'marlins', 'padres', 'tigers', 'bluejays', 'nationals', 'astros', 'royals', 'braves', 'mariners', 'twins', 'mets', 'yankees', 'athletics', 'giants', 'cardinals', 'devilrays', 'rangers'],
-    // teams: ["BREWERS", "ORIOLES", "REDS", "ANGELS", "PHILLIES", "DIAMONDBACKS", "REDSOX", "INDIANS", "DODGERS", "PIRATES", "CUBS", "WHITESOX", "ROCKIES", "MARLINS", "PADRES", "TIGERS", "BLUEJAYS", "NATIONALS", "ASTROS", "ROYALS", "BRAVES", "MARINERS", "TWINS", "METS", "YANKEES", "ATHLETICS", "GIANTS", "CARDINALS", "DEVILRAYS", "RANGERS"],
     word: '',
-    available:["abcdefghijklmnopqrstuvwxyz"],
+    // list of available letters to use
+    available: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
     lettersDiv: undefined,
     incorrectLettersDiv: undefined,
     strikes: undefined,
@@ -19,102 +19,102 @@ var game = {
     correctGuesses: [],
     usedLetters: [],
     guessesLeft: [9],
+    wins: 0,
+    losses: 0,
 
-    
-    startGame:  function () {
-        //pick word
-        // set word
+
+    startGame: function () {
+
         this.lettersDiv = document.getElementById('letters');
         this.incorrectLettersDiv = document.getElementById('incorrect-letters');
-        console.log(guessesLeft);
-        
-       
-        // gereates random word from list of teams
-        var word = game.teams[Math.floor(Math.random() * game.teams.length)];;
+        var word = game.teams[Math.floor(Math.random() * game.teams.length)];
         this.word = word;
         console.log(word);
-    
         this.render();
 
         // // capture user input, and store it in letter 
         document.onkeyup = function (letter) {
-            game.guess(letter.key);
-            console.log(letter);
-        }
+            if (game.guessesLeft >=1) {
+                game.guess(letter.key);    
+        } else {
+            this.render();
+            };
+        };
     },
 
     guess: function (letter) {
         if (this.word.indexOf(letter) > -1) {
             this.correctGuess(letter);
             game.usedLetters.push(letter);
-           console.log(game.usedLetters); 
+            // this.compare();
         } else {
             this.incorrectGuess(letter);
             game.usedLetters.push(letter);
             // strikes left in the game
-            game.guessesLeft--;
-            console.log(game.guessesLeft);
+            this.guessesLeft--;
+            if (game.guessesLeft === 0) {
+                this.youLose(letter);
+                this.render();
+            }
         }
         this.render();
         // wirtes used letters across the bottom of page
-        document.querySelector('#x').innerHTML = game.usedLetters;   
+        document.querySelector('#x').innerHTML = game.usedLetters;
+        document.querySelector('#guessesLeft').innerHTML = ("You have " + game.guessesLeft + " STRIKES Left");
     },
+// plays crying sound and adds one to losses var
+    youLose: function (letter) {
+        lose.play();
+        this.losses++;
+    },
+
 
     correctGuess: function (letter) {
         this.correctGuesses.push(letter);
-        // if(available.indexOf(letter) > -1) {
-
-        console.log(letter, "is a match");
+        hitSound.play();
     },
-    
+
     incorrectGuess: function (letter) {
-        console.log(letter, "is incorrect")
         this.incorrectGuesses.push(letter);
+        strikeSound.play();
     },
 
     render: function () {
         this.renderLettersDiv();
         this.incorrectLettersDiv.innerHTML = '';
-        this.saveToLocalStorage();
     },
+// joins correctGuesses
+    compare: function(){
+        var joined = this.correctGuesses.join('');
+        console.log(joined);
+        console.log(this.word);
+        //     crowd.play();
+},
 
     renderLettersDiv: function () {
         this.lettersDiv.innerHTML = '';
         var html = '';
-            for (var i = 0; i < this.word.length; i++) {
-                if (this.correctGuesses.indexOf(this.word[i]) > -1) {
-                    html += "<div class='correct-letter'>" + this.word[i] + "<div>";
-                } else {
-                    html += "<div class='unguessed-letter'>_</div>";
-                }
+        for (var i = 0; i < this.word.length; i++) {
+            if (this.correctGuesses.indexOf(this.word[i]) > -1) {
+                html += "<span class='correct-letter'>" + this.word[i] + "</span>";
+            } else {
+                html += "<span class='unguessed-letter'>_</span>";
             }
+        }
         this.lettersDiv.innerHTML = html;
+    
     },
-
-    saveToLocalStorage: function () {
-        localStorage.setItem('game', JSON.stringify(this));
-    }
-
+    
 }
-game.startGame();
+$("#start").on("click", function () {
+    game.startGame();
+    window.onload = setup();
 
+    /* buttons */
+    // document.getElementById("restart").onclick = setup;
 
-
-//     if (userGuess) {
-//         // if guess is a valid letter? if so move on else erro
-//         if (availLetters.indexOf(userGuess) > -1) {
-//             // has it been guessed before
-//             if ((lettersMatched && lettersMatched.indexof(userGuess) > -1) || (userGuess && userGuess.indexof(userGuess) > -1)) {
-//             output.innerHTML = '"' + userGuess.toUpperCase() + '"' + messages.guessed;
-//             output.classList.add("warning");
-//             }
-//             // does user Guess already exist if so add to letters already matches, if final letter added, game over with win message
-//             else if (targetWord.indexof(userGuess) >-1) {
-//                 var letterToShow;
-//                 letterToShow = document.querySelectorAll (".letter" + userGuess.toUpperCase());
-//             }
-//          }
-// usedLetters.push(userGuess);
-// // alert(usedLetters);
-// console.log(usedLetters);
-// document.querySelector('#x').innerHTML = usedLetters;
+    // /* reset letter to guess on click */
+    // guessInput.onclick = function () {
+    //     this.value = '';
+    // };
+});
